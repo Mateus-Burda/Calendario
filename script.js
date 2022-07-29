1de17110b6a75a324b80378ee4c47e28dc3b7fab
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-07-29 18:36:20 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,71 +1,104 @@
-let dataHoje = new Date(); //retorna a data de hoje
-let anoAtual = dataHoje.getFullYear(); //retorna o ano atual
-
-//Algoritmo para escrever o mês atual dinamicamente
-let mesAtualNumero =  new Date().getMonth()+1; //precisa do +1 por que a contagem dos meses começa em janeiro como sendo o mês 0.
-let mesAtualEscrito;
+let data = new Date(); //retorna a data de hoje
+let dataHoje = {
+    dia : data.getDate(),
+    ano : data.getFullYear(),
+    mes: data.getMonth()+1,
+    nomeDoMes : definirMes(),
+    diaDaSemana: definirDia(),
+    quantosDiasTem : diasDoMes(),
+}
+console.log(dataHoje);
+function definirDia(){
+    let dia = data.getDay();
+    switch (dia) {
+        case 0: return "Domingo";
+        break;
+        case 1: return "Segunda";
+        break;
+        case 2: return "Terça";
+        break;
+        case 3: return "Quarta";
+        break;
+        case 4: return "Quinta";
+        break;
+        case 5: return "Sexta";
+        break;
+        case 6: return "Sábado";
+        break;  
+    }
+}
+//Função para escrever o mês atual dinamicamente
+function definirMes(){
+let mesAtualNumero = data.getMonth()+1; //precisa do +1 por que a contagem dos meses começa em janeiro como sendo o mês 0.
 switch (mesAtualNumero) {
-        case 1: mesAtualEscrito = "Janeiro";
+        case 1: return "Janeiro";
         break;
-        case 2: mesAtualEscrito = "Fevereiro";
+        case 2: return "Fevereiro";
         break;
-        case 3: mesAtualEscrito = "Março";
+        case 3: return "Março";
         break;
-        case 4: mesAtualEscrito = "Abril";
+        case 4: return "Abril";
         break;
-        case 5: mesAtualEscrito = "Maio";
+        case 5: return "Maio";
         break;
-        case 6: mesAtualEscrito = "Junho";
+        case 6: return "Junho";
         break;
-        case 7: mesAtualEscrito = "Julho";
+        case 7: return "Julho";
         break;
-        case 8: mesAtualEscrito = "Agosto";
+        case 8: return "Agosto";
         break;
-        case 9: mesAtualEscrito = "Setembro";
+        case 9: return "Setembro";
         break;
-        case 10: mesAtualEscrito = "Outubro";
+        case 10: return "Outubro";
         break;
-        case 11: mesAtualEscrito = "Novembro";
+        case 11: return "Novembro";
         break;
-        case 12: mesAtualEscrito = "Dezembro";
+        case 12: return "Dezembro";
         break;
 }
+}
+
 let mesElemento = document.getElementById("mes");
-mesElemento.innerHTML = mesAtualEscrito;
+mesElemento.innerHTML = dataHoje.nomeDoMes + "  "+ dataHoje.ano;
 //Fim do algoritmo para escrever o mês atual dinamicamente
-
 //Algoritmo para definir quantidade de dias no mês
-let quantidadeDias;
-switch (mesAtualNumero) {
-    case 1: quantidadeDias = 31;
+function diasDoMes(){
+switch (data.getMonth()+1) {
+    case 1: return ; 31;
     break;
-    case 2: if ((anoAtual%4)!=0){quantidadeDias=28}else{quantidadeDias = 29;} //if para calcular se o ano é bissexto ou não
+    case 2: if ((dataHoje.ano%4)!=0){return 28}else{return 29;} //if para calcular se o ano é bissexto ou não
     break;
-    case 3: quantidadeDias = 31;
+    case 3: return 31;
     break;
-    case 4: quantidadeDias = 30;
+    case 4: return 30;
     break;
-    case 5: quantidadeDias = 31;
+    case 5: return 31;
     break;
-    case 6: quantidadeDias = 30;
+    case 6: return 30;
     break;
-    case 7: quantidadeDias = 31;
+    case 7: return 31;
     break;
-    case 8: quantidadeDias = 31;
+    case 8: return 31;
     break;
-    case 9: quantidadeDias = 30;
+    case 9: return 30;
     break;
-    case 10: quantidadeDias = 31;
+    case 10: return 31;
     break;
-    case 11: quantidadeDias = 30;
+    case 11: return 30;
     break;
-    case 12: quantidadeDias = 31;
+    case 12: return 31;
     break;
 }
-let a= 0
-for (let index = 0; index < quantidadeDias; index++) {
-    
 }
+//Algoritmo para adicionar a quantidade de dias do mês
+for (let index = 1; index <= dataHoje.quantosDiasTem; index++) {
+    let novoH = document.createElement('h4')
+    let numeroDosDias = document.getElementById("dias-numero");
+    novoH.innerHTML = index;
+    numeroDosDias.appendChild(novoH);
+}
+//Fim do algoritmo
+
 
 
 
