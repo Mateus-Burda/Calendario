75313343e4a351b158be8147351e0bfb2823ff8a
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-08-05 16:51:04 -0300

    refatoração do script

--- script.js
+++ script.js
@@ -1,158 +1,118 @@
 let escolhaAno = 2022;
 let escolhaMes = 1;
+const escolhaDia = 01;
 
-let data = new Date(escolhaAno,escolhaMes); //retorna a data de hoje
-let dataHoje = {
-    dia : data.getDate(),
-    ano : data.getFullYear(),
-    mes: data.getMonth()+1,
-    nomeDoMes : definirMes(),
-    diaDaSemana: definirDia(),
-    quantidadeDeDias : quantosDias(),
-}
 
+var data = new Date(escolhaAno,escolhaMes-1,escolhaDia);
+console.log(data)
+
+//função do botão
 let btnProximo=document.getElementById("btn-proximo");
-btnProximo.onclick = function proxMes() {
+btnProximo.onclick = function proxMes(){
     escolhaMes++;
-    if(escolhaMes>=13){
-        escolhaMes=0;
-        escolhaAno++;
-    }
-    else{
-    let data = new Date(escolhaAno,escolhaMes-1);
-    dataHoje = {
-        dia : data.getDate(),
-        ano : data.getFullYear(),
-        mes: escolhaMes,
-        nomeDoMes : definirMes(),
-        diaDaSemana: definirDia(),
-        quantidadeDeDias : quantosDias(),
-    }
+    data = new Date(escolhaAno,escolhaMes-1,escolhaDia);
     inserirMes();
-    adicionarDias();
+    inserirDias();
 }
-}
+;;
+//fim botão
 
-function calcularDia() {
-    if (data.getDate(0)!=data.getDay()) {
-        for (let index = 1; index < data.getDay(); index++) {
-            let novoTd = document.createElement('td')
-            let numeroDosDias = document.getElementById("semana1");
-            numeroDosDias.appendChild(novoTd);
-    }
-}}
-calcularDia();
-
-function definirDia(){
-    let dia = data.getDay();
-    switch (dia) {
-        case 0: return "Domingo";
-        break;
-        case 1: return "Segunda";
-        break;
-        case 2: return "Terça";
-        break;
-        case 3: return "Quarta";
-        break;
-        case 4: return "Quinta";
-        break;
-        case 5: return "Sexta";
-        break;
-        case 6: return "Sábado";
-        break;  
-    }
-}
-//Função para escrever o mês atual dinamicamente
-function definirMes(){
-switch (escolhaMes) {
-        case 1: return "Janeiro";
-        break;
-        case 2: return "Fevereiro";
-        break;
-        case 3: return "Março";
-        break;
-        case 4: return "Abril";
-        break;
-        case 5: return "Maio";
-        break;
-        case 6: return "Junho";
-        break;
-        case 7: return "Julho";
-        break;
-        case 8: return "Agosto";
-        break;
-        case 9: return "Setembro";
-        break;
-        case 10: return "Outubro";
-        break;
-        case 11: return "Novembro";
-        break;
-        case 12: return "Dezembro";
-        break;
-}
-}
+//função para inserir o mês selecionado
 let inserirMes = function() {
     let mesElemento = document.getElementById("mes");
-mesElemento.innerHTML = dataHoje.nomeDoMes + "  "+ dataHoje.ano;
+    let nomeDoMes;
+    switch (data.getMonth()) {
+        case 0: nomeDoMes = "Janeiro"
+        break;
+        case 1: nomeDoMes = "Fevereiro"
+        break;
+        case 2: nomeDoMes = "Março"
+        break;
+        case 3: nomeDoMes = "Abril"
+        break;
+        case 4: nomeDoMes = "Maio"
+        break;
+        case 5: nomeDoMes = "Junho"
+        break;
+        case 6: nomeDoMes = "Julho"
+        break;
+        case 7: nomeDoMes = "Agosto"
+        break;
+        case 8: nomeDoMes = "Setembro"
+        break;
+        case 9: nomeDoMes = "Outubro"
+        break;
+        case 10: nomeDoMes = "Novembro"
+        break;
+        case 11: nomeDoMes = "Dezembro"
+        break;
+
+    }
+    mesElemento.innerHTML = nomeDoMes + "  "+ data.getFullYear();
 }
 inserirMes();
-//Fim do algoritmo para escrever o mês atual dinamicamente
-//Algoritmo para definir quantidade de dias no mês
-function quantosDias(){
-switch (escolhaMes-1) {
-    case 0: return 31;
-    break;
-    case 1: return 28;
-    break;
-    case 2: return 31;
-    break;
-    case 3: return 30;
-    break;
-    case 4: return 31;
-    break;
-    case 5: return 30;
-    break;
-    case 6: return 31;
-    break;
-    case 7: return 31;
-    break;
-    case 8: return 30;
-    break;
-    case 9: return 31;
-    break;
-    case 10: return 30;
-    break;
-    case 11: return 31;
-    break;
-}
-}
-let count=0;
-let semanaAtual = 1;
 
-//Algoritmo para adicionar a quantidade de dias do mês
-function adicionarDias() {
-    while (count>0) {
-    let td = document.getElementById("td"+count);
-    let semana = document.getElementById("semana"+semanaAtual);
-    semana.removeChild(td);
-    if(count==29){semanaAtual--;}        
-    if(count==22){semanaAtual--;}  
-    if(count==15){semanaAtual--;}        
-    if(count==8){semanaAtual--;}        
-    count--
+let semanaAtual = 1;
+let count=0;
+//função para inserir os dias
+let inserirDias = function() {
+    //inserir os dias
+    let quantidadeDeDias;
+    switch (data.getMonth()) {
+        case 0: quantidadeDeDias = 31;
+        break;
+        case 1: quantidadeDeDias = 28;
+        break;
+        case 2: quantidadeDeDias = 31;
+        break;
+        case 3: quantidadeDeDias = 30;
+        break;
+        case 4: quantidadeDeDias = 31;
+        break;
+        case 5: quantidadeDeDias = 30;
+        break;
+        case 6: quantidadeDeDias = 31;
+        break;
+        case 7: quantidadeDeDias = 31;
+        break;
+        case 8: quantidadeDeDias = 30;
+        break;
+        case 9: quantidadeDeDias = 31;
+        break;
+        case 10: quantidadeDeDias = 30;
+        break;
+        case 11: quantidadeDeDias = 31;
+        break;
+        
     }
-for (let index = 1; index <= dataHoje.quantidadeDeDias; index++) {
-    count++
-    let novoTd = document.createElement('td')
-    novoTd.setAttribute("id","td"+index);
-    if(index==8){semanaAtual++}
-    if(index==15){semanaAtual++}
-    if(index==22){semanaAtual++}
-    if(index==29){semanaAtual++}
-    let semana = document.getElementById("semana"+semanaAtual);
-    novoTd.innerHTML = index;
-    semana.appendChild(novoTd);
+    while(count>0){
+        let tdNovo = document.getElementById("dia"+count);
+        let semana = document.getElementById("semana"+semanaAtual);
+        semana.removeChild(tdNovo)
+        count--
+        if(count==7){semanaAtual--}
+        if(count==14){semanaAtual--}
+        if(count==21){semanaAtual--}
+        if(count==28){semanaAtual--}
+        if(count==35){semanaAtual--}
+    }
+    for(let index = 1; index<=quantidadeDeDias+data.getDay()-1;index++){
+        count++
+        if(index==8){semanaAtual++}
+        if(index==15){semanaAtual++}
+        if(index==22){semanaAtual++}
+        if(index==29){semanaAtual++}
+        if(index==36){semanaAtual++}
+        let semana = document.getElementById("semana"+semanaAtual);
+        let novoTd = document.createElement('td');
+        novoTd.setAttribute("id","dia"+index)
+        semana.appendChild(novoTd);
+        if(index>data.getDay()-1){
+            novoTd.innerHTML= index-data.getDay()+1;
+        }
+    }
 }
-}
-adicionarDias();
-//Fim do algoritmo
+inserirDias();
+
+
+
